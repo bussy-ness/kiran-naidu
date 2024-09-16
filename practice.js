0bd759a2e1d017c83900971f4dec9b9ef999aed8
bussy-ness <vtcreations.bussy01@gmail.com>
2024-09-16 20:28:11 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,16 @@
+
+
+
+
+let image1 = document.getElementById("btn");
+
+
+let image2 = document.getElementById("btn-2");
+
+
+image1.onclick = function(){
+    document.getElementById("myImg").src = "images/img-1.jpg";
+}
+image2.onclick = function(){
+    document.getElementById("myImg").src = "images/img-2.jpg";
+}
